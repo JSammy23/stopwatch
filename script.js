5fdd7b1e7f23a779b85c95e114aa45dac693b9bb
Jordan <jordansampson23@gmail.com>
2023-01-12 12:23:07 -0600

    Watch object created

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const Watch = {
-    
+    milliseconds = 0
+    seconds = 0
+    minutes = 0
+    const start = () => {
+        milliseconds += 10
+        if (milliseconds == 1000){
+            milliseconds = 0
+            seconds++
+            if (seconds == 60){
+                seconds = 0
+                minutes++
+            }
+        }
+    }
+
+    const display = document.querySelector('.display')
+
 }
 
 
 const startBtn = document.getElementById('start')
 const resetBtn = document.getElementById('reset')
+
+startBtn.addEventListener('click', Watch.start)
