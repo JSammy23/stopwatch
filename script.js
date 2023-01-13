2a439c7eef786ad681ffd709e4c5a54d7752d915
Jordan <jordansampson23@gmail.com>
2023-01-12 20:23:47 -0600

    startTimer & updateDisplay fn added

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
-const Watch = {
-    milliseconds = 0
-    seconds = 0
-    minutes = 0
-    const start = () => {
-        milliseconds += 10
-        if (milliseconds == 1000){
-            milliseconds = 0
-            seconds++
-            if (seconds == 60){
-                seconds = 0
-                minutes++
-            }
+const resetBtn = document.querySelector('.reset')
+const startBtn = document.getElementById('start')
+const recordBtn = document.querySelector('.record')
+const display = document.querySelector('.display')
+let minutesDiv = document.querySelector('.minutes')
+let secondsDiv = document.querySelector('.seconds')
+let tensDiv = document.querySelector('.tens')
+let tens = 0
+let seconds = 0
+let minutes = 0
+
+function startTimer(){
+    tens += 10
+    if (tens == 1000){
+        tens = 0
+        seconds++
+        if(seconds == 60){
+            seconds = 0
+            minutes++
         }
     }
-
-    const display = document.querySelector('.display')
-
+    updateDisplay()
 }
 
+startBtn.addEventListener('click', () => {
+    setInterval(startTimer, 10)
+    
+})
 
-const startBtn = document.getElementById('start')
-const resetBtn = document.getElementById('reset')
+const updateDisplay = () => {
+    if (tens <= 9){
+        tensDiv.textContent = '0' + tens
+    } else {
+        tensDiv.textContent = tens
+    } 
+    if (seconds <= 9){
+        secondsDiv.textContent = '0' + seconds
+    } else {
+        secondsDiv.textContent = seconds
+    } 
+    if (minutes <= 9){
+        minutesDiv.textContent = '0' + minutes
+    } else {
+        minutesDiv.textContent = minutes
+    }
+}
 
-startBtn.addEventListener('click', Watch.start)
+// let myInterval = setInterval(myTimer, 1000);
+// let i = 0;
+// function myTimer() {
+//   document.querySelector('.tens').innerHTML = i;
+//   i++;
+// }
